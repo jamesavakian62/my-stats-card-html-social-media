954ae34f3d4197194c8e0f0b267af86c5b3b6969
James L Avakian <92414210+jamesavakian62@users.noreply.github.com>
2021-11-20 09:27:15 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("hello world");
+
+console.log(getElementById('fullname').innerText) = dog.name)
+
+consol.log(dog.name)
+
+// How do I change the text color of the name to green?
+// How do I access the name?
+// How do I change the style?
+// How do I change the value of the style?  with DOM
+
+
+
+// document.querySelector('h1', container).style.color = 'green';
+
+// document.getElementById("myH1").style.color = "green";
+
+// /////style.backgroundImage 
+
+// How do I change my profile picture to steve's?
+
+// How do I access the image?
+// What property do I want to change?
+// What do I want to change that property too?
+
+let dog = {
+    name: "Steve",
+    age: 1,
+    location: "Japan",
+    profileImg: "https://images.unsplash.com/photo-1613410180211-106788bd2d94?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80",
+    bannerImg: "url('https://thumbs.dreamstime.com/b/green-polka-dot-background-25383307.jpg')",
+    backgroundColor: "#00FFFF",
+    stats: {
+        followers: "1.2M",
+        likes: "8M",
+        posts: "2K"
+    }
+}
+document.getElementsbyClassName('profile').innerHTML = "https://images.unsplash.com/photo-1613410180211-106788bd2d94?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80",
+"
+
+
+//How do I get the profile picture of the dog?
